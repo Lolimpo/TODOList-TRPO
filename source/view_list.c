9f593cc3e0d348ae4dcaad18a43dbce16d7fee07
gbggtw <car3@mail.ru>
2018-06-01 18:51:54 +0700

    Add some change

--- source/view_list.c
+++ source/view_list.c
@@ -5,10 +5,26 @@ void view_list()
     char str[100];
     char *estr;
     system("clear");
-    puts("Your decided jobs:");
+    puts("Your decided jobs:\n");
     list = fopen("Todo-List.txt", "r");
     if (list == NULL || (estr = fgets(str, 100, list)) == NULL)
         printf("You have no current plans");
+    else
+    {
+		rewind(list);
+        while(!feof(list))
+        {
+            estr = fgets(str, sizeof(str), list);
+            if(estr != NULL)
+                printf("%s", str);
+        }
+        fclose(list);
+    }
+    puts("\n");
+    puts("Your completed tasks:\n");
+    list = fopen("Done-List.txt", "r");
+    if (list == NULL || (estr = fgets(str, 100, list)) == NULL)
+        printf("You haven't done any jobs");
     else
     {
 		rewind(list);
