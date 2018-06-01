81d3294af49c422afba8aced9697f1e0678821ec
TheLol1mpo <thelol1mpo@gmail.com>
2018-06-01 16:08:13 +0700

    Menu fix

--- source/menu.c
+++ source/menu.c
@@ -126,8 +126,7 @@ void delete_list()
 
 int menu()
 {
-    char m;
-    char n;
+    int m;
     while(1)
     {
         puts("1. View your TODO-List");
@@ -136,25 +135,28 @@ int menu()
         puts("4. Delete tasks");
         puts("0. Exit program");
         puts("What would you like to do next?");
-        scanf("%s",&m);
+        scanf("%d%*c",&m);
         switch(m)
         {
-            case '1': view_list(); break;
-            case '2': add_task(); break;
-            case '3': mark_done(); break;
-            case '4': 
-            system("clear");
-            puts("1. Delete 1 task");
-            puts("2. Delete all current tasks");
-            puts("0. Return to main menu");
-            scanf("%s",&n);
-            switch(n)
-			{
-				case '1': delete_task(); menu(); break;
-				case '2': delete_list(); menu(); break;
-				case '0': system("clear"); menu(); break;
-			}
-            case '0': return 0;
+            case 1:
+                view_list(); break;
+            case 2:
+                add_task(); break;
+            case 3:
+                mark_done(); break;
+            case 4:
+                system("clear");
+                puts("1. Delete 1 task");
+                puts("2. Delete all current tasks");
+                puts("0. Return to main menu");
+                scanf("%d%*c",&m);
+                switch(m)
+                {
+                    case '1': delete_task(); menu(); break;
+                    case '2': delete_list(); menu(); break;
+                    case '0': system("clear"); menu(); break;
+                }
+            case 0: return 0;
             default : puts("No such command");
         }
     }
