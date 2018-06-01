3f4ee1e78e80e0fc1b3c9818c5ba49434a9dfd96
DarkRise12 <darkrisenik@gmail.com>
2018-06-01 17:43:48 +0700

    Change of header file

--- source/todolib.h
+++ source/todolib.h
@@ -1,6 +1,14 @@
 #define TODOLIB_H_
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+FILE *list;
 int menu();
 void add_task();
-void view_list (FILE *input);
+void view_list();
+void mark_done();
+void delete_task();
+void delete_list();
+
+
